d3b4078b1383f4b05e138551524ae2fe1c2bf9ab
Philipp Baum <philipp.baum@me.com>
2012-10-04 15:33:32 +0200

    added lightbox and exif (not final)

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 import cherrypy, os, glob, HelpClasses
 from PIL import Image
+<<<<<<< HEAD
+=======
+import EXIF
+
+>>>>>>> Added jQuery and Lightbox 2 to Gallery, Added Exif data reader (not final)
 class PortfolioPhil(object):
 
 	#Unexposed Help Class
@@ -19,6 +24,7 @@ class PortfolioPhil(object):
 			# convert to thumbnail image
 			im.thumbnail((200, 200), Image.ANTIALIAS)
 
+<<<<<<< HEAD
 			#file = file[:-4] + "_thumb"
 
 			# prefix thumbnail file with T_
@@ -36,6 +42,23 @@ class PortfolioPhil(object):
 			if  not files.endswith("_thumb.jpg"):
 				continue;
 		
+=======
+			# prefix thumbnail file and save
+			im.save(file + "_thumb.jpg", "JPEG")
+			return
+	CreateThumbnail.exposed = False
+	
+	#Checks if there are thumbnails to create and if yes it creates them
+	def CreateThumbnailsOutOfThumbOrdner(self):
+		
+		#Get the image files from the directory, only jpg right now
+		for files in glob.glob("./createThumbnails/*.jpg"):
+			self.CreateThumbnail(files)
+		
+			if  not files.endswith("_thumb.jpg"):
+				continue;
+		
+>>>>>>> Added jQuery and Lightbox 2 to Gallery, Added Exif data reader (not final)
 	CreateThumbnailsOutOfThumbOrdner.exposed = False
 
 	def template(self, body):
@@ -47,9 +70,15 @@ class PortfolioPhil(object):
 		layoutHtml = layoutHtml.replace("${gallery}", self.GetGalleryLinks())
 		return layoutHtml
 
+<<<<<<< HEAD
 	def index(self):
 		
 		self.CreateThumbnailsOutOfThumbOrdner()
+=======
+	def index(self):
+		
+		self.CreateThumbnailsOutOfThumbOrdner()
+>>>>>>> Added jQuery and Lightbox 2 to Gallery, Added Exif data reader (not final)
 		
 		return self.template("Hello PortfolioPhil!")
 	index.exposed = True
@@ -80,17 +109,34 @@ class PortfolioPhil(object):
 	blog.exposed = True
 
 	def gallery(self, folder):
-		outputHtml = ""
+		outputHtml = "<div class='imageRow'>"
 
 		#Get the image files from the directory, only jpg right now
 		for files in glob.glob("./content_portfolio/" + folder + "/*.jpg"):
+<<<<<<< HEAD
 			#self.CreateThumbnail(files)
 
 			if  not files.endswith("_thumb.jpg"):
 				continue;
 			
 			outputHtml += "<img style='padding: 5px' src='" + files + "'>"
+=======
+>>>>>>> Added jQuery and Lightbox 2 to Gallery, Added Exif data reader (not final)
 
+			if  not files.endswith("_thumb.jpg"):
+				continue;
+
+			#f = open(files.replace("_thumb.jpg",""), 'rb')
+			#tags = EXIF.process_file(f, strict=True)
+			#for tag in tags.keys():
+			#	print ("Key:"+ tag + " data: " + str(tags[tag]))
+
+
+			outputHtml += "<div class='single'>"
+			outputHtml += "<a href='" + files.replace("_thumb.jpg","") + "' rel='lightbox[gallery]' title='title'><img src='" + files + "'></a>"
+			outputHtml +="</div>"
+
+		outputHtml += "</div>"
 		return self.template(outputHtml)
 	gallery.exposed = True
 
@@ -100,4 +146,3 @@ class PortfolioPhil(object):
 
 #cherrypy.config.update('conf/portfoliophil.config')
 cherrypy.quickstart(PortfolioPhil(), '/', 'portfoliophil.config')
-
